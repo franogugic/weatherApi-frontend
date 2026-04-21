b9744d1cc930ab7f2bcaa50a68018fa52e09ad66
franogugic <franogugic@icloud.com>
2026-04-21 11:29:46 +0200

    feat: add context for forecast and meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { ForecastProvider } from "@/features/get-weather-forecast/model/forecast-context"
 import { LocationProvider } from "@/features/selected-location/model/location-context"
 import { TimezoneProvider } from "@/features/selected-timezone/model/timezone-context"
-import { DashboardPage } from "./pages/dashboard/ui/DashboardPage"
 import { AppLayout } from "./shared/ui/app-layout/AppLayout"
+import { DashboardPage } from "./pages/dashboard/ui/DashboardPage"
 
 function App() {
 
   return (
     <LocationProvider>
       <TimezoneProvider>
-        <AppLayout >
-          <DashboardPage />
-        </AppLayout>
+        <ForecastProvider>
+          <AppLayout >
+            <DashboardPage />
+          </AppLayout>
+        </ForecastProvider>
       </TimezoneProvider>
     </LocationProvider>
   )
---
+++ src/pages/forecast/ui/ForecastPage.tsx
@@ -0,0 +1,7 @@
+export function ForecastPage() {
+    return (
+        <div className="flex-1 bg-div rounded-4xl p-6">
+            ForecastPage
+        </div>
+    )
+}
